f77067d4d5942b29dc63860cb8bb65a9246195e7
dhamsayar-art <dhamsayar@gmail.com>
2026-07-27 14:14:14 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+const supabaseUrl = "https://oozvolizhiyfuqbynlsm.supabase.co";
+
+const supabaseKey = "PASTE_YOUR_PUBLISHABLE_KEY_HERE";
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
