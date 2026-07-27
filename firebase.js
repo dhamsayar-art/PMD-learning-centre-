eacae6d164e322a809436b1bd53c73b7de44102e
dhamsayar-art <dhamsayar@gmail.com>
2026-07-27 11:24:50 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2CYY9qO_1XTxLzEw3j3mpv4EfQMLcH64",
+  authDomain: "pmd-learning-centre-6b4a5.firebaseapp.com",
+  projectId: "pmd-learning-centre-6b4a5",
+  storageBucket: "pmd-learning-centre-6b4a5.firebasestorage.app",
+  messagingSenderId: "191436199922",
+  appId: "1:191436199922:web:ec23b8dd0e7a7178de3d6b"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
